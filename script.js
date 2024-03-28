994166a75885395378019c621e24b1eafebd4f7d
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-28 08:44:57 +0100

    Add function to validate password keys
    The function checkValidPasswordKey will return true if the key entered
    in the password field is A-Za-z0-9#_-!+&

--- script.js
+++ script.js
@@ -6,16 +6,58 @@ const form = document.querySelector("form");
 let password = ""
 let passwordConfirm = ""
 
+const checkValidPasswordKey = function(keypress) {
+    // Refactor
+    if (
+        keypress.keyCode >= 65 && keypress.keyCode <= 90 || // a-z A-Z
+        keypress.keyCode >= 37 && keypress.keyCode <= 40 || // Arrow keys
+        keypress.keyCode === 8 || // backspace
+        keypress.keyCode === 9 ||// tab
+        keypress.keyCode === 189
+    ) {
+        return true
+    }
+    if (!keypress.shiftKey) {
+        if (
+            keypress.keyCode >= 48 && keypress.keyCode <= 57 ||
+            keypress.keyCode >= 96 && keypress.keyCode <= 105 ||
+            keypress.keyCode === 107 || // Numpad /
+            keypress.keyCode === 109 || // Numpad-
+            keypress.keyCode === 111 // Numpad +
+        ) {
+            return true
+        }           
+    } else {
+        if (
+            keypress.keyCode === 49 || // !
+            keypress.keyCode === 51 || // #
+            keypress.keyCode === 54 || // &
+            keypress.keyCode === 55 // /
+        ) {
+            return true
+        }
+    }
+}
+
 const inputSave = function(e) {
+    if(checkValidPasswordKey(e) || e.kodeCode === 9  ) {
+        //document.querySelector(".password-error-msg").textContent = ""
+        console.log("correct key")
+    } else {
+        e.preventDefault();
+        //document.querySelector(".password-error-msg").textContent = "error"
+    }
+    console.log(e)
     const letterNumber = /^[0-9a-zA-Z]+$/;
-    if(e.target.value.match(letterNumber)) {
+    if(e.key.match(letterNumber)) {
+        console.log("MATCH", e.key)
         e.target.setCustomValidity("")
         if (e.target.id === "password") {
             password = e.target.value;
         } else {
             passwordConfirm = e.target.value;
         }
-    }
+    } 
 }
 
 const checkMatchingPasswords = function(e) {
