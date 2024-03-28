1b6afee70bf04f1850a2ca4fbb681e9b3193c7f2
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-28 21:01:14 +0100

    Validate password with regex
    Check that password contains at least a number, a letter and a given
    range of symbols.

--- script.js
+++ script.js
@@ -5,49 +5,67 @@ const form = document.querySelector("form");
 
 let password = ""
 let passwordConfirm = ""
+const patternLetters = /[a-zA-Z]/
+const patternNumbers = /[0-9]/
+const patternSymbol = /[!+#&/()-]/
 
 const checkValidPasswordKey = function(keypress) {
+    
+    //const pattern = /^[a-zA-Z0–9!#&/()-_]+$/; 
+    if (keypress.key.length === 1) {
+        if (patternLetters.test(keypress.key) ||
+            patternNumbers.test(keypress.key) ||
+            patternSymbol.test(keypress.key)
+        ) {
+            console.log("Pattern", patternSymbol.test(keypress.key))
+            console.log("Number", patternNumbers.test(keypress.key))
+            console.log("Letter", patternLetters.test(keypress.key))
+            return true
+        }
+    }
+    
     if (
-        keypress.keyCode >= 65 && keypress.keyCode <= 90 || // a-z A-Z
         keypress.keyCode >= 37 && keypress.keyCode <= 40 || // Arrow keys
         keypress.keyCode === 8 || // backspace
         keypress.keyCode === 9 ||// tab
-        keypress.keyCode === 13 || // enter
-        keypress.keyCode === 189 // -
+        keypress.keyCode === 13 // enter
+        //keypress.keyCode === 189 // -
     ) {
         return true
     }
     if (!keypress.shiftKey) {
         if (
-            keypress.keyCode >= 48 && keypress.keyCode <= 57 || // Numbers
-            keypress.keyCode >= 96 && keypress.keyCode <= 105 || // Numpad numbers
             keypress.keyCode === 35 || // End
             keypress.keyCode === 36 || // Home 
-            keypress.keyCode === 46 || // Del 
-            keypress.keyCode === 107 || // Numpad /
+            keypress.keyCode === 46  // Del 
+            /* keypress.keyCode === 107 || // Numpad /
             keypress.keyCode === 109 || // Numpad -
-            keypress.keyCode === 111 // Numpad +
+            keypress.keyCode === 111 // Numpad + */
         ) {
             return true
         }           
-    } else {
-        if (
-            keypress.keyCode === 49 || // !
-            keypress.keyCode === 51 || // #
-            keypress.keyCode === 54 || // &
-            keypress.keyCode === 55 // /
-        ) {
-            return true
-        }
     }
+}
+
+const checkValidPasswordString = function(passwordString) {
+    return patternLetters.test(passwordString) && patternNumbers.test(passwordString) && patternSymbol.test(passwordString)
 }
 
 const inputSave = function(e) {
     if(checkValidPasswordKey(e)) {
-        //document.querySelector(".password-error-msg").textContent = ""
-        console.log("correct key")
+        console.log("VALID")
         if (e.target.id === "password") {
             password = e.target.value;
+            if(e.type === "keyup") {
+                console.log(password)
+                if (password.length > 5) {
+                    if (checkValidPasswordString(password)) {
+                        e.target.setCustomValidity("")
+                    } else {
+                        e.target.setCustomValidity("Wrong formatting")
+                    }
+                }
+            }
         } else {
             if(e.key.length === 1 || e.keyCode == 8 || e.keyCode == 46) {
                 e.target.setCustomValidity("")
@@ -55,21 +73,11 @@ const inputSave = function(e) {
             passwordConfirm = e.target.value;
         }
     } else {
+        console.log("forbidden")
         e.preventDefault();
         //document.querySelector(".password-error-msg").textContent = "error"
     }
     //console.log(e)
-    
-    /* const letterNumber = /^[0-9a-zA-Z]+$/;
-    if(e.key.match(letterNumber)) {
-        console.log("MATCH", e.key.length)
-        
-        if (e.target.id === "password") {
-            password = e.target.value;
-        } else {
-            passwordConfirm = e.target.value;
-        }
-    }  */
 }
 
 const checkMatchingPasswords = function(e) {
@@ -88,7 +96,7 @@ const checkMatchingPasswords = function(e) {
         }
     }
 }
-
 passwordFields.forEach(input => input.addEventListener("keydown", inputSave))
+passwordFields.forEach(input => input.addEventListener("keyup", inputSave))
 submitButton.addEventListener("click", checkMatchingPasswords)
 form.addEventListener("submit", (ev) => ev.preventDefault())
