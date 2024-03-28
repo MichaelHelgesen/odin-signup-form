8658e5c6c13d3ae99f82fe43cdddba7ad220643a
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-03-28 10:22:29 +0100

    Correctly style input states
    Overwrite the standard autofill states that inputs get from chrome.

--- script.js
+++ script.js
@@ -7,22 +7,25 @@ let password = ""
 let passwordConfirm = ""
 
 const checkValidPasswordKey = function(keypress) {
-    // Refactor
     if (
         keypress.keyCode >= 65 && keypress.keyCode <= 90 || // a-z A-Z
         keypress.keyCode >= 37 && keypress.keyCode <= 40 || // Arrow keys
         keypress.keyCode === 8 || // backspace
         keypress.keyCode === 9 ||// tab
-        keypress.keyCode === 189
+        keypress.keyCode === 13 || // enter
+        keypress.keyCode === 189 // -
     ) {
         return true
     }
     if (!keypress.shiftKey) {
         if (
-            keypress.keyCode >= 48 && keypress.keyCode <= 57 ||
-            keypress.keyCode >= 96 && keypress.keyCode <= 105 ||
+            keypress.keyCode >= 48 && keypress.keyCode <= 57 || // Numbers
+            keypress.keyCode >= 96 && keypress.keyCode <= 105 || // Numpad numbers
+            keypress.keyCode === 35 || // End
+            keypress.keyCode === 36 || // Home 
+            keypress.keyCode === 46 || // Del 
             keypress.keyCode === 107 || // Numpad /
-            keypress.keyCode === 109 || // Numpad-
+            keypress.keyCode === 109 || // Numpad -
             keypress.keyCode === 111 // Numpad +
         ) {
             return true
@@ -40,24 +43,33 @@ const checkValidPasswordKey = function(keypress) {
 }
 
 const inputSave = function(e) {
-    if(checkValidPasswordKey(e) || e.kodeCode === 9  ) {
+    if(checkValidPasswordKey(e)) {
         //document.querySelector(".password-error-msg").textContent = ""
         console.log("correct key")
+        if (e.target.id === "password") {
+            password = e.target.value;
+        } else {
+            if(e.key.length === 1 || e.keyCode == 8 || e.keyCode == 46) {
+                e.target.setCustomValidity("")
+            }
+            passwordConfirm = e.target.value;
+        }
     } else {
         e.preventDefault();
         //document.querySelector(".password-error-msg").textContent = "error"
     }
-    console.log(e)
-    const letterNumber = /^[0-9a-zA-Z]+$/;
+    //console.log(e)
+    
+    /* const letterNumber = /^[0-9a-zA-Z]+$/;
     if(e.key.match(letterNumber)) {
-        console.log("MATCH", e.key)
-        e.target.setCustomValidity("")
+        console.log("MATCH", e.key.length)
+        
         if (e.target.id === "password") {
             password = e.target.value;
         } else {
             passwordConfirm = e.target.value;
         }
-    } 
+    }  */
 }
 
 const checkMatchingPasswords = function(e) {
